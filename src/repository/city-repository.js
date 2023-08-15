a85d46e59fa13e3c0e727b272a7c6148f562c224
Raze100 <rawatrohit084@gmail.com>
2023-08-15 15:18:58 +0530

    added getAll api in City API

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -56,6 +56,16 @@ class CityRepository{
         }
 
     }
+
+    async getAllCities(){
+        try {
+            const cities = await City.findAll(); 
+            return cities;
+        } catch (error) {
+            console.log("Something went wrong in Repository layer4");
+            throw{error};
+        }
+    }
 }
 
 module.exports = CityRepository;
